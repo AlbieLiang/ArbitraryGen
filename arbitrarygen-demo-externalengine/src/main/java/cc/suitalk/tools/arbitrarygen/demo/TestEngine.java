f20e6adf582636f6a13bac82b235eb71a87de5ce
albieliang <albieliang@gmail.com>
2016-11-06 23:19:18 +0800

    【ArbitraryGen】添加自定义Engine demo

---
+++ arbitrarygen-demo-externalengine/src/main/java/cc/suitalk/tools/arbitrarygen/demo/TestEngine.java
@@ -0,0 +1,44 @@
+package cc.suitalk.tools.arbitrarygen.demo;
+
+import net.sf.json.JSONObject;
+
+import java.util.Map;
+
+import cc.suitalk.arbitrarygen.extension.AGCore;
+import cc.suitalk.arbitrarygen.extension.ArbitraryGenEngine;
+import cc.suitalk.arbitrarygen.extension.ArbitraryGenProcessor;
+import cc.suitalk.arbitrarygen.utils.Log;
+
+/**
+ * Created by albieliang on 16/11/6.
+ */
+public class TestEngine implements ArbitraryGenEngine {
+
+    private static final String TAG = "AG.TestEngine";
+
+    @Override
+    public String getName() {
+        return "testEngine";
+    }
+
+    @Override
+    public String[] getDependencies() {
+        return new String[0];
+    }
+
+    @Override
+    public void onError(int errorCode, String message) {
+        Log.e(TAG, "execute engine error, code is '%d', message is '%s'", errorCode, message);
+    }
+
+    @Override
+    public JSONObject exec(AGCore core, Map<String, ArbitraryGenProcessor> processors, JSONObject args) {
+        Log.i(TAG, "execute engine(%s), args(%s)", getName(), args);
+        return null;
+    }
+
+    @Override
+    public void initialize(AGCore core, JSONObject args) {
+        Log.i(TAG, "initialize engine(%s), args(%s)", getName(), args);
+    }
+}
